7f0d0c787cb08e52237bec815a34cb4c58347e1e
guio12 <g.audouze@gmail.com>
2018-04-22 19:03:50 +0200

    fin de quête crud

---
+++ src/AppBundle/Entity/Site.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Site
+ *
+ * @ORM\Table(name="site")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\SiteRepository")
+ */
+class Site
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=128)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="icao", type="string", length=4)
+     */
+    private $icao;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="lattitude", type="float")
+     */
+    private $lattitude;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="longitude", type="float")
+     */
+    private $longitude;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="city", type="string", length=128)
+     */
+    private $city;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Site
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set icao
+     *
+     * @param string $icao
+     *
+     * @return Site
+     */
+    public function setIcao($icao)
+    {
+        $this->icao = $icao;
+
+        return $this;
+    }
+
+    /**
+     * Get icao
+     *
+     * @return string
+     */
+    public function getIcao()
+    {
+        return $this->icao;
+    }
+
+    /**
+     * Set lattitude
+     *
+     * @param float $lattitude
+     *
+     * @return Site
+     */
+    public function setLattitude($lattitude)
+    {
+        $this->lattitude = $lattitude;
+
+        return $this;
+    }
+
+    /**
+     * Get lattitude
+     *
+     * @return float
+     */
+    public function getLattitude()
+    {
+        return $this->lattitude;
+    }
+
+    /**
+     * Set longitude
+     *
+     * @param float $longitude
+     *
+     * @return Site
+     */
+    public function setLongitude($longitude)
+    {
+        $this->longitude = $longitude;
+
+        return $this;
+    }
+
+    /**
+     * Get longitude
+     *
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     *
+     * @return Site
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+}
+
